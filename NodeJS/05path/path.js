d0d42e6931d8218e4415d67492faf673bea04d2b
一只会爬的小呆瓜 <2020885569@qq.com>
2023-10-04 16:13:58 +0800

    Node.js：add path模块

---
+++ NodeJS/05path/path.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+let pathname = 'D:/program file/nodejs/node.exe';
+
+//获取路径分隔符
+console.log(path.sep); // '\'
+
+//拼接绝对路径
+console.log(path.resolve(__dirname,'test'));
+
+//解析路径
+console.log(path.parse(pathname));
+
+//获取路径基础名称
+console.log(path.basename(pathname));
+
+//获取路径的目录名
+console.log(path.dirname(pathname));
+
+//获取路径的拓展名
+console.log(path.extname(pathname));
