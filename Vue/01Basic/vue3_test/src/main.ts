bf64ae4dffcb99cdc876b29db0029bf9d54f65db
一只会爬的小呆瓜 <2020885569@qq.com>
2024-02-02 02:01:02 +0800

    Delete Vue/01Basic directory

--- Vue/01Basic/vue3_test/src/main.ts
+++
@@ -1,9 +0,0 @@
-// 引入样式
-import './assets/main.css'
-
-// 创建Vue应用
-import { createApp } from 'vue'
-// 引入App组件
-import App from './App.vue'
-// 创建应用实例设置App为根组件，并挂载App组件到#app元素上
-createApp(App).mount('#app')
