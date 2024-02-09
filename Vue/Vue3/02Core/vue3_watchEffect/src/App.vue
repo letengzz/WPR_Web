50ee981fb4a17562da4f623c1944111a479aab50
letengzz <2020885569@qq.com>
2024-02-09 15:19:02 +0800

    ADD vue3-watchEffect

---
+++ Vue/Vue3/02Core/vue3_watchEffect/src/App.vue
@@ -0,0 +1,29 @@
+<!-- 编写模板结构 -->
+<template>
+  <!-- Html -->
+  <div class="app">
+    <h1>你好 世界</h1>
+    <Person/>
+  </div>
+</template>
+<!-- 编写脚本代码 -->
+<script lang="ts">
+// JS或TS
+import Person from "./components/person.vue"
+//默认暴露
+export default {
+  name: "App", //组件名
+  components: { Person } //引入的组件
+}
+
+</script>
+<!-- 样式 -->
+<style scoped>
+/* css */
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
