11ace173aed22e76f7597e989297c02584ffa4fa
letengzz <2020885569@qq.com>
2024-02-09 15:24:20 +0800

    ADD vue3-tag ref

---
+++ Vue/Vue3/02Core/vue3_tagRef/src/App.vue
@@ -0,0 +1,31 @@
+<!-- 编写模板结构 -->
+<template>
+  <!-- Html -->
+  <div class="app">
+    <h1 ref="title2">hello</h1>
+    <button @click="showLog">点我打印内容</button>
+    <Person ref="ren"/>
+  </div>
+</template>
+<!-- 编写脚本代码 -->
+<script setup lang="ts" name="App">
+// JS或TS
+import Person from "./components/person.vue"
+import {ref} from "vue";
+let title2 = ref();
+let ren = ref();
+function showLog() {
+  // console.log(title2.value)
+  console.log(ren.value)
+}
+</script>
+<!-- 样式 -->
+<style scoped>
+/* css */
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
---
+++ Vue/Vue3/02Core/vue3_tagRef/src/components/person.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="person">
+    <h1>你好</h1>
+<!--    <h2 id="title2">前端</h2>-->
+    <h2 ref="title2">前端</h2>
+    <h3>Vue</h3>
+    <button @click="showLog">点我打印内容</button>
+  </div>
+</template>
+
+<script lang="ts" setup name="Person">
+import {ref} from "vue";
+
+//变量名必须和ref中的名称一致
+//创建一个title2,用于存储ref标记的内容
+let title2 = ref()
+let a = ref(0)
+let b = ref(1)
+let c = ref(2)
+function showLog(){
+  // 通过id获取元素
+  // console.log(document.getElementById('title2'));
+  console.log(title2.value);
+}
+// vue中要使用defineExpose暴露内容
+// 使用defineExpose将组件中的数据交给外部
+defineExpose({a,b})
+</script>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  margin: 0 5px;
+}
+</style>
