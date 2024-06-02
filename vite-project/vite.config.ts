81db34d993ebef7683b79967eb64932be61709d9
letengzz <2020885569@qq.com>
2024-06-02 18:47:43 +0800

    Update vite '@'alias

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,13 @@ export default defineConfig({
     port: 9999, //设置端口号
     open: true, //设置自动打开浏览器
     host: '0.0.0.0', //设置IP
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname,'src')
+      }
+    ]
   }
 })
